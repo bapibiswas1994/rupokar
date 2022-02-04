8f72a1410019efb914b1994088b9b9ba70ff4d04
Samrat <rats480@gmail.com>
2022-02-04 13:42:16 +0530

    bootstrap link fix

--- resources/views/layouts/frontend/layout.blade.php
+++ resources/views/layouts/frontend/layout.blade.php
@@ -13,6 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="{{ asset('frontend-assets/images/favicon.png') }}" type="image/gif" sizes="16x16">
 
+    <link rel="stylesheet" type="text/css" href="{{ asset('frontend-assets/css/bootstrap.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('frontend-assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend-assets/fonts/font-awesome.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('frontend-assets/css/style.css') }}">
@@ -25,8 +26,8 @@
     <link rel="stylesheet" type="text/css"
         href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css">
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> --}}
 
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
@@ -50,14 +51,14 @@
     <script type="text/javascript" src="{{ asset('frontend-assets/js/jquery-1.11.3.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('frontend-assets/js/jquery-3.3.1.slim.min.js') }}"></script>
 
-    <!-- <script type="text/javascript" src="{{ asset('frontend-assets/js/bootstrap.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('frontend-assets/js/bootstrap.min.js') }}"></script> -->
-    <!-- <script type="text/javascript" src="{{ asset('frontend-assets/js/popper.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('frontend-assets/js/respond.min.js') }}"></script> -->
+    <script type="text/javascript" src="{{ asset('frontend-assets/js/bootstrap.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('frontend-assets/js/bootstrap.min.js') }}"></script>
+    {{-- <script type="text/javascript" src="{{ asset('frontend-assets/js/popper.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('frontend-assets/js/respond.min.js') }}"></script> --}}
 
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-    </script>
+    </script> --}}
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
